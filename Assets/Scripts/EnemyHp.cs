3ce90367582895b10baa1dc622ec05611f4f02a3
S. Houkes <99656452+sietse9321@users.noreply.github.com>
2023-07-27 18:11:01 +0200

    feat: hp
    player now has hp

--- Assets/Scripts/EnemyHp.cs
+++ Assets/Scripts/EnemyHp.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class EnemyHp : MonoBehaviour
 {
-    [SerializeField] public float hp = 4;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float hp = 4;
     void Update()
     {
         if(hp <= 0)
--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerHP : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float pHp;
+    [SerializeField]EnemyHp enemyHp;
     void Start()
     {
-        
+        //sets the player hp to 200
+        pHp = 200f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Enemy"))
+        {
+            enemyHp = other.GetComponent<EnemyHp>();
+            pHp -= enemyHp.hp;
+            Destroy(other.gameObject);
+        }
+    }
+    private void Update()
+    {
+        print(pHp);
     }
 }

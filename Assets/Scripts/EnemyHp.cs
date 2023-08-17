9502d1e5a45aa25bb629ea4265c098d2748d70fc
S. Houkes <99656452+sietse9321@users.noreply.github.com>
2023-08-17 22:43:23 +0200

    Feat: Money from mobs
    Mobs now give money to the player when they die.

--- Assets/Scripts/EnemyHp.cs
+++ Assets/Scripts/EnemyHp.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class EnemyHp : MonoBehaviour
 {
     public float hp = 4;
+    public int moneyToGive;
+    [SerializeField] PlayerStats pStats;
     void Update()
     {
         if(hp <= 0)
         {
+            pStats = FindObjectOfType<PlayerStats>();
+            pStats.money += moneyToGive;
             Destroy(gameObject);
         }
     }
